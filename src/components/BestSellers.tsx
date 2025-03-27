c21d50bcaf908df8c85fad9cefa0e806db9842b8
Pablo Rossoni <pablorossoni107@outlook.com>
2025-03-26 21:56:48 -0300

    feat: criar componentes BESTSELLERS e BESTSELLINGTSHIRT

---
+++ src/components/BestSellers.tsx
@@ -0,0 +1,21 @@
+// import BestSellingTshirt from "./BestSellingTshirt"
+
+
+const BestSellers = () => {
+  return (
+    <div className="w-full max-w-7xl mx-auto py-10 px-4">
+      <h2 className="text-center text-2xl pb-10 font-oswald">AS CAMISETAS MAIS VENDIDAS DA RP STORE</h2>
+
+      <div className="flex">
+        <h1>Carrossel com as camisetas</h1>
+
+        {/* <BestSellingTshirt />
+        <BestSellingTshirt />
+        <BestSellingTshirt />
+        <BestSellingTshirt /> */}
+      </div>
+    </div>
+  )
+}
+
+export default BestSellers
