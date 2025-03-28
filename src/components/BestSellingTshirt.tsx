6077aa22df77aa8e059de8d3c21ed9e85f9216df
Pablo Rossoni <pablorossoni107@outlook.com>
2025-03-27 22:53:26 -0300

    Feat: Adicionando Props no componente

--- src/components/BestSellingTshirt.tsx
+++ src/components/BestSellingTshirt.tsx
@@ -1,19 +1,26 @@
-import CamisetaQueen from "../assets/images/BestSellingTshirt/camiseta-queen-of-the-stones-age-aberta_495x495+fill_ffffff+crop_center.jpg"
 
-const BestSellingTshirt = () => {
+interface CardTshirt {
+  imageSrc: string;
+  imageAlt: string;
+  title: string;
+  price: string;
+  installmentPrice: string;
+
+}
+const BestSellingTshirt = ({ imageSrc, imageAlt, title, price, installmentPrice }: CardTshirt) => {
   return (
     <article className="flex flex-col items-center">
       <figure>
-        <img src={CamisetaQueen} alt="Camiseta Queens Of The Stone Age" />
-        <figcaption className="text-zinc-800">
-          Camiseta Queens Of The Stone Age
+        <img src={imageSrc} alt={imageAlt} />
+        <figcaption className="text-zinc-800 text-center">
+          {title}
         </figcaption>
       </figure>
 
       <section className="text-center pt-3.5">
-        <p className="text-zinc-800 font-bold">R$ 90,80 no pix</p>
+        <p className="text-zinc-800 font-bold">{price} no pix</p>
         <p className="text-zinc-600">
-          R$ 80,00 em até <strong>2x</strong> de <strong>R$ 40,00</strong>
+          {installmentPrice} <strong>2x</strong>
         </p>
         <p className="text-zinc-600">sem juros</p>
       </section>
