494dde7d49dfaf56730b4a01fdddf3224bee71ac
Pablo Rossoni <pablorossoni107@outlook.com>
2025-03-27 22:52:57 -0300

    feat: Adicionando carrossel ao componente

--- src/components/BestSellers.tsx
+++ src/components/BestSellers.tsx
@@ -1,4 +1,11 @@
-// import BestSellingTshirt from "./BestSellingTshirt"
+import CamisetaQueen from "../assets/images/BestSellingTshirt/camiseta-queen-of-the-stones-age-aberta_495x495+fill_ffffff+crop_center.jpg"
+
+import BestSellingTshirt from "./BestSellingTshirt"
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 
 
 const BestSellers = () => {
@@ -6,14 +13,113 @@ const BestSellers = () => {
     <div className="w-full max-w-7xl mx-auto py-10 px-4">
       <h2 className="text-center text-2xl pb-10 font-oswald">AS CAMISETAS MAIS VENDIDAS DA RP STORE</h2>
 
-      <div className="flex">
-        <h1>Carrossel com as camisetas</h1>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={4}
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+          renderBullet: (index, className) => `
+          <span class="${className} w-4 h-4 mx-1 bg-red-400 opacity-75 rounded-full transition-all 
+            hover:bg-blue-500 hover:scale-110  swiper-pagination-bullet"></span>
+        `,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
 
-        {/* <BestSellingTshirt />
-        <BestSellingTshirt />
-        <BestSellingTshirt />
-        <BestSellingTshirt /> */}
-      </div>
+      >
+        <SwiperSlide>
+          <BestSellingTshirt
+            imageSrc={CamisetaQueen}
+            imageAlt="Camiseta Queen"
+            title="Camiseta Quen Of The Stones"
+            price={"R$80,00"}
+            installmentPrice={"R$90,00"}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <BestSellingTshirt
+            imageSrc={CamisetaQueen}
+            imageAlt="Camiseta Queen"
+            title="Camiseta Quen Of The Stones"
+            price={"R$80,00"}
+            installmentPrice={"R$90,00"}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <BestSellingTshirt
+            imageSrc={CamisetaQueen}
+            imageAlt="Camiseta Queen"
+            title="Camiseta Quen Of The Stones"
+            price={"R$80,00"}
+            installmentPrice={"R$90,00"}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <BestSellingTshirt
+            imageSrc={CamisetaQueen}
+            imageAlt="Camiseta Queen"
+            title="Camiseta Quen Of The Stones"
+            price={"R$80,00"}
+            installmentPrice={"R$90,00"}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <BestSellingTshirt
+            imageSrc={CamisetaQueen}
+            imageAlt="Camiseta Queen"
+            title="Camiseta Quen Of The Stones"
+            price={"R$80,00"}
+            installmentPrice={"R$90,00"}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <BestSellingTshirt
+            imageSrc={CamisetaQueen}
+            imageAlt="Camiseta Queen"
+            title="Camiseta Quen Of The Stones"
+            price={"R$80,00"}
+            installmentPrice={"R$90,00"}
+          />
+        </SwiperSlide>
+        ...
+      </Swiper>
+      {/* <div className="flex">
+
+        <BestSellingTshirt
+          imageSrc={CamisetaQueen}
+          imageAlt="Camiseta Queen"
+          title="Camiseta Quen Of The Stones"
+          price={"R$80,00"}
+          installmentPrice={"R$90,00"}
+        />
+
+        <BestSellingTshirt
+          imageSrc={CamisetaQueen}
+          imageAlt="Camiseta Queen"
+          title="Camiseta Quen Of The Stones"
+          price={"R$80,00"}
+          installmentPrice={"R$90,00"}
+        />
+
+        <BestSellingTshirt
+          imageSrc={CamisetaQueen}
+          imageAlt="Camiseta Queen"
+          title="Camiseta Quen Of The Stones"
+          price={"R$80,00"}
+          installmentPrice={"R$90,00"}
+        />
+
+        <BestSellingTshirt
+          imageSrc={CamisetaQueen}
+          imageAlt="Camiseta Queen"
+          title="Camiseta Quen Of The Stones"
+          price={"R$80,00"}
+          installmentPrice={"R$90,00"}
+        />
+
+
+      </div> */}
     </div>
   )
 }
